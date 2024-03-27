38281c4f7a7792f2b7c2438cd46c7872f36ee800
utkarsh-gupta17 <211220061@nitdelhi.ac.in>
2024-03-27 12:21:48 +0530

    initial backend

--- main.go
+++ main.go
@@ -1,11 +1,55 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	customwebsocket "gochat/websocket"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// )
+
+// func serverWs(pool *customwebsocket.Pool, w http.ResponseWriter, r *http.Request) {
+// 	log.Println("This is working")
+// 	conn, err := customwebsocket.Upgrade(w, r)
+// 	if err != nil {
+// 		log.Println(err)
+// 		return
+// 	}
+
+// 	client := &customwebsocket.Client{
+// 		Conn: conn,
+// 		Pool: pool,
+// 	}
+// 	pool.Register <- client
+// 	client.Read()
+// }
+
+// func setupRoutes() {
+// 	log.Println("This is working")
+// 	pool := customwebsocket.NewPool()
+// 	go pool.Start()
+
+// 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprint(w, "This is Workinggg!!")
+// 		serverWs(pool, w, r)
+// 	})
+// }
+
+//	func main() {
+//		setupRoutes()
+//		port := os.Getenv("PORT")
+//		if port == "" {
+//			port = "10000"
+//		}
+//		log.Println("Listening on port",port)
+//		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",port), nil))
+//	}
 package main
 
 import (
-	"fmt"
 	customwebsocket "gochat/websocket"
 	"log"
 	"net/http"
-	"os"
 )
 
 func serverWs(pool *customwebsocket.Pool, w http.ResponseWriter, r *http.Request) {
@@ -30,17 +74,11 @@ func setupRoutes() {
 	go pool.Start()
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "This is Workinggg!!")
 		serverWs(pool, w, r)
 	})
 }
 
 func main() {
 	setupRoutes()
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "10000"
-	}
-	log.Println("Listening on port",port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",port), nil))
+	http.ListenAndServe(":9000", nil)
 }
